76b2424f91df441f28e99e33b6c3c34f2cd71e8a
pumba299 <pumba299@gmail.com>
2022-07-14 18:04:03 +0200

    added block fresh deals

---
+++ js/burger-menu.js
@@ -0,0 +1,21 @@
+(function () {
+    const iconBurger = document.querySelector(".burger-icon");
+    const navigationsBody = document.querySelector(".contacts-wrapper__header");
+    iconBurger.addEventListener("click", function (e) {
+      document.body.classList.toggle("lock");
+      iconBurger.classList.toggle("active");
+      navigationsBody.classList.toggle("active");
+    });
+  
+    const navLinks = document.querySelectorAll(".navigation__link");
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener("click", (_) => {
+            if (iconBurger.classList.contains("active")) {
+                document.body.classList.remove("lock");
+                iconBurger.classList.remove("active");
+                navigationsBody.classList.remove("active");
+            }
+          }
+      );
+    });
+  })();
