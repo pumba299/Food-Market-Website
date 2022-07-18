707ffda86a6be0239c25f83d32fdb81e85359136
pumba299 <pumba299@gmail.com>
2022-07-18 18:08:34 +0200

    was doing tabs

---
+++ js/nav-tabs.js
@@ -0,0 +1,6 @@
+(function () {
+    const freshDealsTabs = document.querySelector(".fresh-deals__nav-item");
+    freshDealsTabs.addEventListener("click", function (e){
+        document.freshDealsTabs.classList.toggle("");
+    })
+})();
